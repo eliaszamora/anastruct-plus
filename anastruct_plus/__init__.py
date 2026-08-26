ca0ea4344227642e9c6e757177d9b12c301cd568
eliaszamora <37598447+eliaszamora@users.noreply.github.com>
2026-08-26 18:01:44 -0400

    chore: expose v0.2.5 result presentation layer

--- anastruct_plus/__init__.py
+++ anastruct_plus/__init__.py
@@ -1,6 +1,6 @@
 """anaStruct Plus: compact plotting enhancements for anaStruct."""
 
-from .axis_semantics import SystemElements, SystemElementsPlus
+from .result_units_frame import SystemElements, SystemElementsPlus
 
 __all__ = ["SystemElements", "SystemElementsPlus"]
-__version__ = "0.2.4"
+__version__ = "0.2.5"
