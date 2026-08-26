5f47b68fa7d1e22dcc2b5a3f9fb693b8e5ea1ac2
eliaszamora <37598447+eliaszamora@users.noreply.github.com>
2026-08-26 13:51:02 -0400

    Add package entry point

---
+++ anastruct_plus/__init__.py
@@ -0,0 +1,6 @@
+"""anaStruct Plus: compact plotting enhancements for anaStruct."""
+
+from .system import SystemElements, SystemElementsPlus
+
+__all__ = ["SystemElements", "SystemElementsPlus"]
+__version__ = "0.1.0"
